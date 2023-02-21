50dee06df0e4122928aa0f6f8fe6144b5060e2a6
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-02-21 10:58:29 -0300

    componente do rodapé

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
 import Team from './components/Team';
+import Rodape from './components/Rodape';
 
 
 function App() {
@@ -67,6 +68,8 @@ function App() {
           colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
         /> 
       )}
+
+      <Rodape />
     </div>
   );
 }

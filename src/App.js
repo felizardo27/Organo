09254565c528abe5db05a4b6231fd474223955ff
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-02-21 09:46:59 -0300

    Elevando a lista de times

--- src/App.js
+++ src/App.js
@@ -6,6 +6,44 @@ import Team from './components/Team';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    }
+  ]
+
   const [colaboradores, setColaboradores] = useState([])
 
   const novoColaborador = (colaborador) => {
@@ -15,9 +53,18 @@ function App() {
   return (
     <div>
       <Banner />
-      <Form colaboradorCadastrado={colaborador => novoColaborador(colaborador)}/>
-      <Team nome='Programação'/>
-      <Team nome='Front-End'/>
+      <Form 
+        colaboradorCadastrado={colaborador => novoColaborador(colaborador)}
+      />
+
+      {times.map((time) => 
+        <Team 
+          key={time.nome} 
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria} 
+        /> 
+      )}
     </div>
   );
 }
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,9 +1,15 @@
 import './Team.css'
 
 const Team = (props) => {
+    
     return(
-        <section className='team'>
-            <h3>{props.nome}</h3>
+        <section 
+            className='team'
+            style={{backgroundColor: props.corSecundaria}}
+        >
+            <h3 style={{borderColor: props.corPrimaria}}>
+                {props.nome}
+            </h3>
         </section>
     )
 }

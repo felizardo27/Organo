cce9b6b752e0f8d227eb01d98ef35b613573b945
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-02-28 20:40:40 -0300

    refatorando formulario

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,10 +2,10 @@ import './Input.css'
 
 const Input = ({type = 'text', label, placeHolder, valor, aoAlterado, obrigatorio = false}) => {
     return(
-        <div className={`input`}>
+        <div className={`input ${type}`}>
             <label>{label}</label>
             <input
-                type={type ? type : 'text'} 
+                type={type} 
                 value={valor}
                 onChange={(evento) => aoAlterado(evento.target.value)}
                 required={obrigatorio}
